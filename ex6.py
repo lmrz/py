c195c944970e4955d3e0c8b58a9ceaf12f97a03f
lmrz <641655250@qq.com>
2020-09-12 16:56:25 +0800

    new commit

---
+++ ex6.py
@@ -0,0 +1,15 @@
+type_of_people = 10
+x = f"There are {type_of_people} type_of_people."
+
+binary = "binary"
+do_not = "dont't"
+
+y = f"Those who know {binary} and Those who {do_not}."
+
+print(x)
+print(y)
+print(f"I said: {y}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evaluation =
